8808c3bf6578f10f02e67a90e9a5a01d2aa1d981
Giancarlo <gcarlo911@gmail.com>
2019-09-17 21:24:47 -0700

    Added a moving thing
    using the arrow keys.

---
+++ shapes/Assets/MovingThing.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingThing : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public float speed;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position += new Vector3(0, 1 * Time.deltaTime * speed, 0);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position += new Vector3(0, -1 * Time.deltaTime * speed, 0);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += new Vector3(1 * Time.deltaTime * speed, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += new Vector3(-1 * Time.deltaTime * speed, 0, 0);
+
+        if (Input.GetKey(KeyCode.B))
+        {
+            transform.position += new Vector3(0, 0, -1 * Time.deltaTime * speed);
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {       
+            transform.position += new Vector3(0, 0, 1 * Time.deltaTime * speed);
+        }
+        }
+    }
+}
