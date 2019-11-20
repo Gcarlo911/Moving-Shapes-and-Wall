5f4f9c0d5ea049b2567fd640ba08ce8b91f33b8a
Giancarlo <gcarlo911@gmail.com>
2019-11-20 00:00:24 -0800

    ballsHWDone

---
+++ balls/Assets/BallController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    public GameObject Ball1;
+
+    public GameObject Ball2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(Ball1, new Vector3(0, 0, 0), Quaternion.identity);
+        Instantiate(Ball2, new Vector3(1, 0, 0), Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float x;
+        float y;
+        x = 1;
+        y = 1;
+
+        
+        if (Input.GetKeyDown(KeyCode.K)) {
+            Ball1.transform.localScale += new Vector3(x*Time.deltaTime, y*Time.deltaTime, 0);
+        }
+        else Ball1.transform.localScale -= new Vector3(x * Time.deltaTime, y * Time.deltaTime, 0);
+
+        if (x < 1)
+        {
+            Ball1.transform.localScale = new Vector3(1, 1, 0);
+        }
+    }
+}
